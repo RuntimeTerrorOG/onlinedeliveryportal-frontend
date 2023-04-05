d855f8da574810e81889d96e7adcc1a5037fcb94
Isuri99 <isuriiddamalgoda@gmail.com>
2023-04-05 11:20:55 +0530

    changed sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,21 +7,24 @@ import profileImg from '../../assets/images/user-286.png';
 import logo from '../../assets/images/logo.jpeg';
 
 const Sidebar = () => {
+
+    
+   
     return  (
     <div className="sidebar">
         <div className="sidebar__top">
         
-            <h2>
+            
             <div className="logo">
                
                <img src={logo} alt=""/>
               
                </div>
+                   <div className="title">
                     LAUNDRY MART
-
-                    <i className='ri-menu-line'></i>
-            </h2>
-
+                    </div>
+            
+                    </div>
             
             
             <div className="profile">
@@ -35,9 +38,9 @@ const Sidebar = () => {
                 </div>
             
 
-        </div>
+       
 
-        <div className ="sidebar__content">
+        <div className ="sidebar__content" >
             <div className="menu">
                 <ul className="nav__list">
                     {navLinks.map((item,index) => (
@@ -48,9 +51,11 @@ const Sidebar = () => {
                                 navClass.isActive ? "nav__active nav__link" : "nav__link"
                             }
                             >
+                                
                                 <i className={item.icon}></i>
                             
                             {item.Display}
+                            
                         </NavLink>
                     </li>
                     ))}
